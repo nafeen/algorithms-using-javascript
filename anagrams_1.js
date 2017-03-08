38022cfbd22c16844f5b759a366eca2928293636
Abdul Nafeen <abdulnafeen@yahoo.com>
2017-03-08 23:18:36 +0530

    INITIAL COMMIT

---
+++ anagrams_1.js
@@ -0,0 +1,16 @@
+
+/* Given two strings (lowercase a-z), how many characters do we need remove (from either)
+ * to make them anagrams?
+ * EXAMPLE: hello, billion --> ANSWER: 6 ('he' from hello and 'biin' from billion)
+ */
+
+
+// function to input both strings and returns removed characters
+var mainFunction = function (stringA, stringB) {
+
+	// todo: logic
+	
+	return numCharRemoved;
+}
+
+// 
